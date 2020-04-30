d16e51747bf77968ed0b52e10440dd67b1398913
soyandresbernal <soyandresbernal@gmail.com>
2020-04-29 23:30:22 -0500

    code prototype

---
+++ js/app.js
@@ -0,0 +1,43 @@
+/* Prototype
+Todos los objetos en JavaScript
+provienen de Object; 
+todos los objetos heredan 
+métodos y propiedades de Object.prototype
+ */
+
+/* //FUNCION :constructora
+
+function Animal(nombre, genero) {
+  //atributos:
+  this.nombre;
+  this.genero;
+}
+
+// Es conveniente separa metodos de  propiedades del constructor
+
+//Metodo
+Animal.prototype.ruido = function () {
+  console.log("Haciendo ruido");
+};
+
+//Creacion de  Objeto
+const diosDeLaDestruccion = new Animal("Bills", "Felino");
+console.log(diosDeLaDestruccion);
+ */
+//------------------------------
+
+//CLASES
+
+class Animal {
+  constructor(nombre, genero) {
+    this.nombre = nombre;
+    this.genero = genero;
+  }
+  //metoodos
+  ruido() {
+    console.log("Haciendo ruido");
+  }
+}
+
+const diosDeLaDestruccion = new Animal("Bills", "Felino");
+console.log(diosDeLaDestruccion);
